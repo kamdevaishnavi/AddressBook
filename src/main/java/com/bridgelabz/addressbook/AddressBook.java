11c7f35436bdb09e478d6d435decaa2bf47aca10
vaishnavi-kamde <kamdevaishnavi1995@gmail.com>
2023-02-06 16:13:42 +0530

    ADDED :- code for edit contact details

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,37 +1,129 @@
 package com.bridgelabz.addressbook;
 
-import java.util.Scanner;
+import java.util.*;
+public class AddressBook extends Contact{
+    ArrayList<Contact> al1 = new ArrayList();
 
-public class AddressBook {
-    Scanner scanner = new Scanner(System.in);
-    public void CreateContact(){
+    Scanner scan = new Scanner(System.in);
+
+    public Contact addDetails() {
         Contact contact = new Contact();
-        System.out.println("Enter first name");
-        String firstname = scanner.next();
+
+        System.out.print("Enter first name :: ");
+        firstname = scan.next();
         contact.setFirstname(firstname);
-        System.out.println("Enter last name");
-        String lastname = scanner.next();
+
+        System.out.print("Please Enter The Last Name :: ");
+        lastname = scan.next();
         contact.setLastname(lastname);
-        System.out.println("Enter address");
-        String address = scanner.next();
-        contact.setLastname(address);
-        System.out.println("Enter city");
-        String city = scanner.next();
+
+        System.out.print("Please Enter The Address :: ");
+        address = scan.next();
+        contact.setAddress(address);
+
+        System.out.print("Please Enter The City :: ");
+        city = scan.next();
         contact.setCity(city);
-        System.out.println("Enter state");
-        String state = scanner.next();
+
+        System.out.print("Please Enter The State :: ");
+        state = scan.next();
         contact.setState(state);
-        System.out.println("Enter zip");
-        int zip = scanner.nextInt();
-        contact.setZip(zip);
-        System.out.println("Enter phonenum");
-        long phonenum = scanner.nextLong();
-        contact.setPhonenum(phonenum);
-        System.out.println("Enter email");
-        String email = scanner.next();
+
+        System.out.print("Please Enter The email ID :: ");
+        email = scan.next();
         contact.setEmail(email);
-        System.out.println(contact);
 
+        System.out.print("Please Enter The Zip Code :: ");
+        zipCode = scan.nextInt();
+        contact.setZipCode(zipCode);
 
+        System.out.print("Please Enter The Phone Number :: ");
+        phoneNo = scan.nextLong();
+        contact.setPhoneNo(phoneNo);
+
+        // System.out.println(obj);
+
+        al1.add(contact);
+        Iterator itr = al1.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        return contact;
+    }
+
+    void editContact() {
+        Contact contact = null;
+
+        System.out.println("Enter the Contact Name which you want to Edit ");
+        String name = scan.next();
+
+        Iterator<Contact> itr = al1.iterator();
+
+        while (itr.hasNext()) {
+            Contact tempContact = itr.next();
+            if (name.equals(tempContact.firstname)) {
+                contact = tempContact;
+            }
+        }
+        if (contact != null) {
+
+            System.out.println("Enter your choice what you want to edit in contact edit press"
+                    + " \n 1.PhoneNo \n 2.Firstname \n 3.Lastname \n 4.Address \n 5.City \n "
+                    + "6.State \n 7. Email \n 8.ZipCode ");
+
+            int ch = scan.nextInt();
+            switch (ch) {
+
+                case 1:
+                    System.out.print("Please Enter The New Phone Number :: ");
+                    phoneNo = scan.nextLong();
+                    contact.setPhoneNo(phoneNo);
+                    break;
+                case 2:
+                    System.out.print("Enter first name :: ");
+                    firstname = scan.next();
+                    contact.setFirstname(firstname);
+                    break;
+                case 3:
+                    System.out.print("Please Enter The Last Name :: ");
+                    lastname = scan.next();
+                    contact.setLastname(lastname);
+                    break;
+                case 4:
+                    System.out.print("Please Enter The Address :: ");
+                    address = scan.next();
+                    contact.setAddress(address);
+                    break;
+                case 5:
+                    System.out.print("Please Enter The City :: ");
+                    city = scan.next();
+                    contact.setCity(city);
+                    break;
+                case 6:
+                    System.out.print("Please Enter The State :: ");
+                    state = scan.next();
+                    contact.setState(state);
+                    break;
+                case 7:
+                    System.out.print("Please Enter The email ID :: ");
+                    email = scan.next();
+                    contact.setEmail(email);
+                    break;
+                case 8:
+                    System.out.print("Please Enter The Zip Code :: ");
+                    zipCode = scan.nextInt();
+                    contact.setZipCode(zipCode);
+                    break;
+                default:
+                    System.out.println("Invalid input ");
+            }
+
+            System.out.println("Addressbook after editing contact ");
+            al1.forEach(person -> System.out.println(person));
+
+        } else {
+            System.out.println("Name not found ");
+
+        }
     }
 }
--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,9 +1,37 @@
 package com.bridgelabz.addressbook;
-
+import java.util.*;
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to address book program");
-        AddressBook addressBook = new AddressBook();
-        addressBook.CreateContact();
+
+        AddressBook obj = new AddressBook();
+        obj.addDetails();
+        System.out.println("********************************************************"
+                + "******************************************************************");
+
+        String ch;
+        do {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter choice \n 1.Add Contact \n 2.Edit Contact");
+            int choice;
+            choice = scan.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    obj.addDetails();
+                    break;
+                case 2:
+                    obj.editContact();
+                    break;
+
+                default:
+                    System.out.println("Invalid Input");
+            }
+            System.out.println("******************************************************");
+            System.out.println("DO YOU WANT TO ADD MORE CONTACT (PRESS y to contune)");
+            ch = scan.next();
+        } while (ch.equals("y") || ch.equals("Y"));
     }
 }
+
